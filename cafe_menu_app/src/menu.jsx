0621282a4b6f5e2a3c60cad89bb37e08b0c46b14
LAPTOP-8L1MTC96\Afra <omid.elc88@gmail.com>
2025-04-26 00:27:05 +0330

    design menu

--- cafe_menu_app/src/menu.jsx
+++ cafe_menu_app/src/menu.jsx
@@ -1,7 +1,28 @@
 import './menu.css'
 
-function Menu(){
+const Menu = () => {  
+    const menuItems = [  
+        { icon: '🍹', label: 'شربت' },   
+        { icon: '🫖', label: 'دم‌نوش سردنوش' },  
+        { icon: '🍔', label: 'برگر ساندویچ' },  
+        { icon: '🍕', label: 'پیتزا' },  
+        { icon: '🍽️', label: 'غذای اصلی' },  
+        { icon: '🥗', label: 'سالاد' },  
+        { icon: '🍳', label: 'صبحانه' },  
+    ];  
 
-}
+    return (  
+        <div className="menu-container">  
+            {menuItems.map((item, index) => (  
+                <div key={index} className="menu-item">  
+                    <span className="icon">{item.icon}</span>  
+                    <span className="label">{item.label}</span>  
+                </div>  
+            ))}  
+            <div className="arrow left">&lt;</div>  
+            <div className="arrow right">&gt;</div>  
+        </div>  
+    );  
+};
 
 export default Menu;
